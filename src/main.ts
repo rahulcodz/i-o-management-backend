b2a8b867ccef990c2277a62ac53104648cc675ed
Rahul Panchal <rahul@codzgarage.com>
2026-01-05 17:35:06 +0530

    feat: add NestJS application bootstrap, including CORS, validation, exception filters, and Swagger documentation.

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useGlobalFilters(new HttpExceptionFilter());
 
